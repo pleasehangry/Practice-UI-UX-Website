93d9563e3803a0ff47dd495a8de8dc0b20a90997
= <=>
2022-08-17 12:14:42 +0700

    Blog & Footer

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import './Article.module.sass';
+import './Article.css';
 
-const Article = () => {
-    return <div>Article</div>;
+const Article = ({ imgUrl, title, date }) => {
+    return (
+        <div className="gpt3__blog-container_article">
+            <div className="gpt3__blog-container_article-image">
+                <img src={imgUrl} alt="Blog" />
+            </div>
+            <div className="gpt3__blog-container_article-content">
+                <div>
+                    <p>{date}</p>
+                    <h3>{title}</h3>
+                </div>
+                <p>Read Full Article</p>
+            </div>
+        </div>
+    );
 };
 
 export default Article;
